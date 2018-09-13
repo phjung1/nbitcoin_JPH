76be6aeec09560c96d463d7c3eb270715dce87b4
phjung1 <36128943+phjung1@users.noreply.github.com>
2018-09-13 19:50:55 +0900

    초기 커밋.

---
+++ NBitcoin_jph/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace NBitcoin_jph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Key privateKey = new Key();
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine(publicKey);
+
+            Console.WriteLine(Network.Main);
+            Console.WriteLine(Network.TestNet);
+
+        }
+    }
+}
